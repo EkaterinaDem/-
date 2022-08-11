d226f6e203f25effef02efedecb770381b411f40
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-12 00:10:48 +0300

    4.1

--- дз 1/4/Program.cs
+++ дз 1/4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("oboze");
+﻿using static System.Console;
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Clear();
+Write("ВВедите число: ");
+int N = Convert.ToInt32(ReadLine());
+int a = 1;
+while (a <=N)
+{
+    if (a%2 == 0)
+    {
+    WriteLine($"{a}");
+    }
+    a++;
+}
+
