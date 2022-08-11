6ac6795ce0febfea15df628c613d35a17cf2223e
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-11 23:44:05 +0300

    2.1

--- дз 1/2/Program.cs
+++ дз 1/2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+WriteLine("ВВедите первое число ");
+int numberA = Convert.ToInt32(ReadLine());
+WriteLine("ВВедите второе число ");
+int numberB = Convert.ToInt32(ReadLine());
+WriteLine("ВВедите третье число ");
+int numberC = Convert.ToInt32(ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+{
+WriteLine($"Среди чисел {numberA}, {numberB}, {numberC} наибольшее {numberA}");
+}
+if (numberB > numberA && numberB > numberC)
+{WriteLine($"Среди чисел {numberA}, {numberB}, {numberC} наибольшее {numberB}");}
+if (numberC > numberB && numberC > numberA) 
+{
+WriteLine($"Среди чисел {numberA}, {numberB}, {numberC} наибольшее {numberC}");
+}
+
