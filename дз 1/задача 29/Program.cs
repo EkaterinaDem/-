e89cb7a5a381360f9131083905e6da6f7cac6ebe
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-22 03:53:01 +0300

    29

---
+++ дз 1/задача 29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+using static System.Console;
+
+Clear();
+WriteLine("ВВедите 8 чисел: ");
+int[] array = new int[8];
+
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = int.Parse(ReadLine());
+}
+WriteLine($"[{String.Join(",", array)}]");
