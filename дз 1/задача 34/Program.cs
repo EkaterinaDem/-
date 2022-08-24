7997dac35d1239008a8995e3a66d49163f731e60
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-25 02:01:41 +0300

    34

--- дз 1/задача 34/Program.cs
+++ дз 1/задача 34/Program.cs
@@ -1,4 +1,5 @@
-﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
 
 [345, 897, 568, 234] -> 2
 */
@@ -6,6 +7,28 @@
 using static System.Console;
 
 Clear();
-WriteLine("ВВедите 8 чисел: ");
+int [] array = GetArray(4, 100, 1000);
+WriteLine($"[{String.Join(",", array)}]");
+int CountOfEven = findEven(array);
+WriteLine ($"Количество четных элементов массива = {CountOfEven}");
 
+int [] GetArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i=0; i<size;i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
 
+int findEven (int[] array)
+{
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    if (array[i]%2==0)
+    {
+        count+=1;
+    }
+    return count;
+}
