a109a2855cd28e94371834a8a3af268e4bb5b814
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-25 08:11:45 +0300

    38

--- дз 1/задача 38/Program.cs
+++ дз 1/задача 38/Program.cs
@@ -4,5 +4,30 @@
 */
 
 using static System.Console;
-
 Clear();
+
+int[] array = GetArray(10, -99, 99);
+WriteLine($"[{String.Join(",", array)}]");
+int SumOfUnevenIndex = sum(array);
+WriteLine($"Сумма элементов нечетных позиций массива = {SumOfUnevenIndex}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
+
+int sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i <= array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
