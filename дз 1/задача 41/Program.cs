58055a3f8bbc19a8ab4631bfbc1c70acb28f83cd
Ekaterina Demchenko <9992112588@mail.ru>
2022-08-29 19:39:42 +0300

    41

---
+++ дз 1/задача 41/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+using static System.Console;
+using System.Linq;
+
+Clear();
+
+
+WriteLine("ВВедите M чисел: ");
+int[] array = new int[m];
+
+int count = 0;
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = int.Parse(ReadLine());
+}
+
+if (i>0)
+{
+    count = count+1;
+}
+
+WriteLine($"Количество положительных элементов: {count}");
